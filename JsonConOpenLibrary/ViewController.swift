1a32523ffc6c8339d204a9c93b33a021aaba20d4
aixpec <aixpec@gisystemsint.com>
2016-10-29 13:10:15 -0500

    Initial Commit

---
+++ JsonConOpenLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JsonConOpenLibrary
+//
+//  Created by Andrés Ixpec on 29/10/16.
+//  Copyright © 2016 Andrés Ixpec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
